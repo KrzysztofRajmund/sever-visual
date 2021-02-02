7882f7d69b9cb88ff11bbca7fb75893db8699bfd
Krzysztof Nadlonek <knadlonek@gmail.com>
2021-02-02 23:39:13 +0100

    added assets

--- src/components/Home/LandingPage.jsx
+++ src/components/Home/LandingPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import DigitalImage from "../../assets/digital-image.jpg";
+import DigitalImageThree from "../../assets/digital-image3.JPG";
+import DigitalImageTwo from "../../assets/digital-image2.jpg";
 
 const LandingPage = () => {
   return (
@@ -8,13 +9,13 @@ const LandingPage = () => {
         <div className="landing-page__digital" hover-digital="цифровой">
           <h1>Digital</h1>
         </div>
-        <img class="artist-card__img" src={DigitalImage} alt="St. Vincent" />
+        <img class="artist-card__img" src={DigitalImageThree} alt="St. Vincent" />
       </div>
       <div class="artist-card">
         <div className="landing-page__analog" hover-analog="аналог">
           <h1>Analog</h1>
         </div>
-        <img class="artist-card__img" src={DigitalImage} alt="St. Vincent" />
+        <img class="artist-card__img" src={DigitalImageTwo} alt="St. Vincent" />
       </div>
     </div>
   );
